c4ddddb02502f650f4f785d6d56e3a6411a6d90a
Bilyana Konstantinova <bkonstantinova@melontech.com>
2022-06-07 13:13:03 +0300

    Initial commit - created models, forms and function-based views

--- sofia_weather/urls.py
+++ sofia_weather/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from sofia_weather import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     path('subscribe/', views.SubscribeUserView.as_view(), name='subscribe'),
 ]
--- sofia_weather/views.py
+++ sofia_weather/views.py
@@ -9,51 +9,51 @@ from sofia_weather.forms import SubscribedUsersForm
 from sofia_weather.models import SubscribedUsers
 
 
-# class IndexView(TemplateView):
-#     template_name = 'index.html'
-#
-#     def get_context_data(self, **kwargs):
-#         city = 'Sofia'
-#         city_lat = '42.6951'
-#         city_lon = '23.325'
-#         API_key = '24eede1f3b0f51f76e7f4597fbf3fc1b'
-#         source_url = f'https://api.openweathermap.org/data/2.5/' \
-#                      f'onecall?lat={city_lat}&lon={city_lon}&units=metric&appid={API_key}'
-#
-#         city_weather = requests.get(source_url).json()
-#         print(city_weather)
-#
-#         current_weather = {
-#             'city': city,
-#             'temperature': city_weather['current']['temp'],
-#             'feels_like': city_weather['current']['feels_like'],
-#             'humidity': city_weather['current']['humidity'],
-#             'description': city_weather['current']['weather'][0]['description'],
-#             'weather_icon': city_weather['current']['weather'][0]['icon'],
-#             'min_temp': city_weather['daily'][0]['temp']['min'],
-#             'max_temp': city_weather['daily'][0]['temp']['max'],
-#             'date': datetime.now(),
-#         }
-#
-#         weather_weekly = {
-#             'daily_forecast': city_weather['daily'][1:],
-#
-#         }
-#
-#         for day in weather_weekly.values():
-#             days = 1
-#             for d in day:
-#                 current_time_date = datetime.now() + timedelta(days=days)
-#                 current_time = current_time_date.strftime('%A, %d %b, %Y')
-#                 d.update({'date': current_time})
-#                 days += 1
-#
-#         context = {
-#             'current_weather': current_weather,
-#             'weather_weekly': weather_weekly,
-#         }
-#
-#         return context
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        city = 'Sofia'
+        city_lat = '42.6951'
+        city_lon = '23.325'
+        API_key = '24eede1f3b0f51f76e7f4597fbf3fc1b'
+        source_url = f'https://api.openweathermap.org/data/2.5/' \
+                     f'onecall?lat={city_lat}&lon={city_lon}&units=metric&appid={API_key}'
+
+        city_weather = requests.get(source_url).json()
+        print(city_weather)
+
+        current_weather = {
+            'city': city,
+            'temperature': city_weather['current']['temp'],
+            'feels_like': city_weather['current']['feels_like'],
+            'humidity': city_weather['current']['humidity'],
+            'description': city_weather['current']['weather'][0]['description'],
+            'weather_icon': city_weather['current']['weather'][0]['icon'],
+            'min_temp': city_weather['daily'][0]['temp']['min'],
+            'max_temp': city_weather['daily'][0]['temp']['max'],
+            'date': datetime.now(),
+        }
+
+        weather_weekly = {
+            'daily_forecast': city_weather['daily'][1:],
+
+        }
+
+        for day in weather_weekly.values():
+            days = 1
+            for d in day:
+                current_time_date = datetime.now() + timedelta(days=days)
+                current_time = current_time_date.strftime('%A, %d %b, %Y')
+                d.update({'date': current_time})
+                days += 1
+
+        context = {
+            'current_weather': current_weather,
+            'weather_weekly': weather_weekly,
+        }
+
+        return context
 
 
 def index(request):
