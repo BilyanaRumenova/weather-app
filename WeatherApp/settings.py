63af2eb5946f2b9a4c12ed64646e95185f4b8ab3
Bilyana Konstantinova <bil.konstantinova@gmail.com>
2022-07-21 12:27:41 +0300

    updates for eb

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['WeatherApp-dev.eu-central-1.elasticbeanstalk.com']
 
 INTERNAL_IPS = [
     "127.0.0.1",
